8938bc5e85aa409f0613fddf319baab282c2fa7b
Supraja-SP <129285507+Supraja-SP@users.noreply.github.com>
2023-05-07 13:43:23 +0200

    Create 1.DNA_FastQC.sh

---
+++ 1.DNA_FastQC.sh
@@ -0,0 +1,4 @@
+# Performing FASTQC on Trimmed DNA Samples for both SRR4342129 and SRR4342133
+
+fastqc SRR4342129_1.paired.trimmed.fastq.gz SRR4342129_2.paired.trimmed.fastq.gz
+fastqc SRR4342133_1.paired.trimmed.fastq.gz SRR4342133_2.paired.trimmed.fastq.gz
