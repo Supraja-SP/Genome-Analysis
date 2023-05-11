a4e723fff6339c9a8a217ebf31697159a9e0938b
Supraja-SP <129285507+Supraja-SP@users.noreply.github.com>
2023-05-11 21:06:45 +0200

    Update 01_fastqc.sh

--- 01_fastqc.sh
+++ 01_fastqc.sh
@@ -1,13 +1,13 @@
 #### Performing FASTQC on Trimmed DNA Samples for both SRR4342129 and SRR4342133 ###
 
 # FastQC was performed on interactive node
-salloc -A uppmax2023-2-8 -M snowy -p core -n 2 -t 20:00 
+salloc -A uppmax2023-2-8 -M snowy -p core -n 2 -t 40:00 
 
 # Load modules
 module load bioinfo-tools
 module load FastQC
 
-# Creating directories
+# Creating DNA directories
 mkdir -p /home/supraja/Genome_project/DNA_data
 
 # Copying the raw reads as a link 
@@ -19,3 +19,16 @@ cd /home/supraja/Genome_project/DNA_data
 # Performing fastqc
 fastqc SRR4342129_1.paired.trimmed.fastq.gz SRR4342129_2.paired.trimmed.fastq.gz
 fastqc SRR4342133_1.paired.trimmed.fastq.gz SRR4342133_2.paired.trimmed.fastq.gz
+
+# Creating RNA directories
+mkdir -p /home/supraja/Genome_project/RNA_data
+
+# Copying the raw reads as a link 
+ln -s /proj/genomeanalysis2023/Genome_Analysis/3_Thrash_2017/RNA_untrimmed
+
+# Navigating to directory
+cd /home/supraja/Genome_project/RNA_data
+
+# Performing fastqc
+fastqc SRR4342137.1.fastq.gz SRR4342137.2.fastq.gz
+fastqc SRR4342139.1.fastq.gz SRR4342139.2.fastq.gz
