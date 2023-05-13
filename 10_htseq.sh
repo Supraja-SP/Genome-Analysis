c239d440b75d507de051070f56bbb6f8a3aa12d3
Supraja-SP <129285507+Supraja-SP@users.noreply.github.com>
2023-05-14 00:33:14 +0200

    Create 10_htseq.sh

---
+++ 10_htseq.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -A uppmax2023-2-8
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 6:00:00
+#SBATCH -J HTSEQ_29
+#SBATCH --mail-user supraja.sathya-prakash.7461@student.uu.se
+#SBATCH --mail-type=ALL
+
+# Loading modules
+module load bioinfo-tools
+module load htseq
+
+prokka_bin29=/home/supraja/Genome_project/DNA_data/Prokka/Prokka29_bins
+bam_sorted_dir1=/home/supraja/Genome_project/RNA_data/RNA_mapping_BWA/RNA_mapped_to_location1
+out_dir=/home/supraja/Genome_project/DNA_data/HTseq/annotated_bins29
+
+for bin in $bam_sorted_dir1/*.bam
+do
+        bin_dir_name=$(basename "$bin" _sorted.bam)
+        htseq-count -f bam -r pos -t CDS -i ID --stranded=no ${bin} <(sed '/^##FASTA/Q' ${prokka_bin29}/${bin_dir_name}/${bin_dir_name}.gff) > ${out_dir}/${bin_dir_name}_counts.out
+done
