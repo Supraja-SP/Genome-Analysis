f21addcdd7a59495fb71ddc03818ab52270700d9
Supraja-SP <129285507+Supraja-SP@users.noreply.github.com>
2023-05-16 19:20:59 +0200

    Create bin33_qa_plot.sh

---
+++ bin33_qa_plot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2023-2-3
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 6:00:00
+#SBATCH -J bin_stat29
+#SBATCH --mail-user supraja.sathya-prakash.7461@student.uu.se
+#SBATCH --mail-type=ALL
+
+module load bioinfo-tools
+module load CheckM/1.0.12
+
+out_folder=/home/supraja/Genome_project/DNA_data/CheckM/CheckM_33_results/
+bin_folder=/home/supraja/Genome_project/DNA_data/MetaBat/bins33/
+plot_folder=/home/supraja/Genome_project/DNA_data/CheckM/CheckM_33_results/plots/
+
+checkm bin_qa_plot $out_folder -x fa $bin_folder $plot_folder
